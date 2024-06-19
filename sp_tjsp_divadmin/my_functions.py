b92505be8b50bc4d4586ef88b35bd2b98b8ab190
Michel Metran <michelmetran@gmail.com>
2024-06-18 22:51:34 -0300

    Ajustes

--- sp_tjsp_divadmin/my_functions.py
+++ sp_tjsp_divadmin/my_functions.py
@@ -4,6 +4,12 @@ abr/2023
 """
 
 import re
+import re
+import unicodedata
+
+import numpy as np
+import pandas as pd
+import xlrd
 
 
 def keep_numbers(my_string):
@@ -25,6 +31,89 @@ def find_text_between_parenthesis(my_string):
 
     """
     return my_string[my_string.find("(") + 1 : my_string.find(")")]
+
+
+def remove_multiple_spaces(my_string: str) -> str:
+    """
+    _summary_
+    https://stackoverflow.com/questions/1546226/is-there-a-simple-way-to-remove-multiple-spaces-in-a-string
+
+    :param my_string: _description_
+    :type my_string: _type_
+    :return: _description_
+    :rtype: _type_
+    """
+    return re.sub(r'\s\s+', ' ', my_string)
+
+
+def remove_accents(my_string: str) -> str:
+    """
+    _summary_
+
+    :param input_str: _description_
+    :type input_str: _type_
+    :return: _description_
+    :rtype: _type_
+    """
+    nfkd_form = unicodedata.normalize('NFKD', my_string)
+    only_ascii = nfkd_form.encode('ASCII', 'ignore')
+    return only_ascii
+
+
+def strip_accents(my_string: str) -> str:
+    """
+    Retira acentos das palavras
+    https://stackoverflow.com/questions/517923/what-is-the-best-way-to-remove-accents-normalize-in-a-python-unicode-string
+
+    :param my_string: Texto com acento
+    :type my_string: string
+    :return: Texto sem acento
+    :rtype: my_string
+    """
+    return ''.join(
+        c
+        for c in unicodedata.normalize('NFD', str(my_string))
+        if unicodedata.category(c) != 'Mn'
+    )
+
+
+def adjust_columns(df: pd.DataFrame, column_ajust: str) -> pd.DataFrame:
+    """
+    Adiciona coluna à tabela, com sufixo _temp,
+    com correções diversas para fins de "fazer bater"
+
+    :param df: tabela bruta
+    :return: tabela com coluna a mais
+    """
+    #
+    dd_fix = {
+        # Errado / Certo
+        'rio grande de serra': 'rio grande da serra',
+        'santa rosa de viterbo': 'santa rosa do viterbo',
+        'santana do parnaiba': 'santana de parnaiba',
+        'sao luis do paraitinga': 'sao luiz do paraitinga',
+    }
+
+    # Coluna para ajustar
+    col_temp = f'{column_ajust}_temp'
+
+    # Para Bater
+    df[col_temp] = df[column_ajust]
+    df[col_temp] = df[col_temp].str.strip()
+    df[col_temp] = df[col_temp].str.lower()
+    df[col_temp] = df.apply(
+        lambda x: strip_accents(x[col_temp]), axis='columns'
+    )
+    df[col_temp] = df[col_temp].str.replace('’', '')
+    df[col_temp] = df[col_temp].str.replace('´', '')
+    df[col_temp] = df[col_temp].str.replace("'", '')
+    df[col_temp] = df[col_temp].rename(dd_fix, axis='rows')
+    df = df.replace({col_temp: dd_fix})
+
+    # Results
+    df.info()
+    df.head()
+    return df
 
 
 if __name__ == '__main__':
--- sp_tjsp_divadmin/my_paths.py
+++ sp_tjsp_divadmin/my_paths.py
@@ -3,7 +3,6 @@ Pastas do Projeto
 mar.23
 """
 
-
 from pathlib import Path
 
 project_path = Path(__file__).parents[1]
@@ -13,8 +12,8 @@ module_path = Path(__file__).parents[0]
 data_path = module_path / 'data'
 data_path.mkdir(exist_ok=True)
 
-#input_path = data_path / 'input'
-#input_path.mkdir(exist_ok=True)
+# input_path = data_path / 'input'
+# input_path.mkdir(exist_ok=True)
 
 output_path = data_path / 'output'
 output_path.mkdir(exist_ok=True)
